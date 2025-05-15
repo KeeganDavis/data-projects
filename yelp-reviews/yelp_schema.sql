6542d33feb098371bab7b1f4045ad467970bd81e
KeeganDavis <keegan.davis96@gmail.com>
2025-05-15 15:46:18 -0500

    changed ordering of columns to flow better

--- yelp-reviews/yelp_schema.sql
+++ yelp-reviews/yelp_schema.sql
@@ -14,14 +14,14 @@ FILE_FORMAT = (TYPE = JSON);
 CREATE OR REPLACE TABLE yelp_reviews AS
 SELECT 
     reviews_text:business_id::STRING AS business_id,
-    reviews_text:cool::NUMBER AS cool_votes,
-    reviews_text:date::DATE AS review_date,
-    reviews_text:funny::NUMBER AS funny_votes,
     reviews_text:review_id::STRING AS review_id,
-    reviews_text:stars::NUMBER AS stars,
+    reviews_text:user_id::STRING AS user_id,
+    reviews_text:date::DATE AS review_date,
     reviews_text:text::STRING AS user_review,
-    reviews_text:useful::NUMBER AS useful_votes,
-    reviews_text:user_id::STRING AS user_id
+    reviews_text:stars::NUMBER AS stars,
+    reviews_text:cool::NUMBER AS cool_votes,
+    reviews_text:funny::NUMBER AS funny_votes,
+    reviews_text:useful::NUMBER AS useful_votes
 FROM yelp_reviews_json;
 
 -- create table to hold yelp businesses json
@@ -39,17 +39,17 @@ FILE_FORMAT = (TYPE = JSON);
 -- break businesses json into columns and rows
 CREATE OR REPLACE TABLE yelp_businesses AS
 SELECT
+    businesses_text:name::STRING AS name,
     businesses_text:business_id::STRING AS business_id,
+    businesses_text:is_open::NUMBER AS is_open,
+    businesses_text:review_count::NUMBER AS review_count,
+    businesses_text:stars::NUMBER AS stars,
     businesses_text:categories::STRING AS categories,
     businesses_text:state::STRING AS state,
     businesses_text:city::STRING AS city,
-    businesses_text:is_open::NUMBER AS is_open,
-    businesses_text:latitude::FLOAT AS latitude,
-    businesses_text:longitude::FLOAT AS longitude,
-    businesses_text:name::STRING AS name,
     businesses_text:postal_code::STRING AS postal_code,
-    businesses_text:review_count::NUMBER AS review_count,
-    businesses_text:stars::NUMBER AS stars
+    businesses_text:latitude::FLOAT AS latitude,
+    businesses_text:longitude::FLOAT AS longitude
 FROM yelp_businesses_json;
 
 -- create table to hold yelp users json
@@ -66,7 +66,12 @@ FILE_FORMAT = (TYPE = JSON);
 
 CREATE OR REPLACE TABLE yelp_users AS
 SELECT
+    users_text:name::STRING AS first_name,
+    users_text:user_id::STRING AS user_id,
+    users_text:yelping_since::DATE AS yelping_since,
+    users_text:review_count::NUMBER AS review_count,
     users_text:average_stars::FLOAT AS avg_stars,
+    users_text:fans::NUMBER AS fans,
     users_text:compliment_cool::NUMBER AS cool_compliments,
     users_text:compliment_cute::NUMBER AS cute_compliments,
     users_text:compliment_funny::NUMBER AS funny_compliments,
@@ -80,13 +85,8 @@ SELECT
     users_text:compliment_writer::NUMBER AS writer_compliments,
     users_text:cool::NUMBER AS cool_votes,
     users_text:elite::STRING AS elite,
-    users_text:fans::NUMBER AS fans,
     users_text:funny::NUMBER AS funny_votes,
-    users_text:name::STRING AS first_name,
-    users_text:review_count::NUMBER AS review_count,
-    users_text:useful::NUMBER AS useful_votes,
-    users_text:user_id::STRING AS user_id,
-    users_text:yelping_since::DATE AS yelping_since
+    users_text:useful::NUMBER AS useful_votes
 FROM yelp_users_json;
 
 -- create table to hold yelp tips json
@@ -104,8 +104,8 @@ FILE_FORMAT = (TYPE = JSON);
 CREATE OR REPLACE TABLE yelp_tips AS
 SELECT
     tips_text:business_id::STRING AS business_id,
-    tips_text:compliment_count::NUMBER AS compliment_count,
+    tips_text:user_id::STRING AS user_id,
     tips_text:date::DATE AS date,
     tips_text:text::STRING AS tip,
-    tips_text:user_id::STRING AS user_id,
+    tips_text:compliment_count::NUMBER AS compliment_count
 FROM yelp_tips_json;
